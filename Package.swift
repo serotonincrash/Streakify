20c028242f7790229c5534e3bbfaee12ea04e6e9
Sean Wong <wong_sean@defence.gov.sg>
2024-11-26 15:37:55 +0800

    chore: bump up deployment targets

--- Package.swift
+++ Package.swift
@@ -5,10 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Streakify",
-    platforms: [.iOS(.v15),
-                .macOS(.v15),
-                .watchOS(.v6),
-                .tvOS(.v15)],
+    platforms: [.iOS(.v17),
+                .macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
